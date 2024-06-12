e0a3751b1e2ceea4bb6687a073287bf9de2cfa6d
Naman Sharma <namansharma232009@gmail.com>
2024-06-12 16:09:55 +0530

    idk

--- Andrew.js
+++
@@ -1,1 +0,0 @@
-location.href = "https://youtube.com/shorts/r28fmvr7MbQ?feature=share";
--- Main.js
+++ Main.js
@@ -1,1 +1,26 @@
-console.log('amogus')
+$(document).ready(() => {
+    $.getJSON("https://api.ipify.org?format=json", function (data) {
+        const ip = data.ip;
+        $("#gfg").html(ip);
+
+        $.getJSON(`https://ipapi.co/${ip}/json/`, function (locationData) {
+            const latitude = locationData.latitude;
+            const longitude = locationData.longitude;
+
+            const map = L.map('mapid').setView([latitude, longitude], 13);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '© OpenStreetMap'
+            }).addTo(map);
+
+            L.marker([latitude, longitude]).addTo(map)
+                .bindPopup(`<b>Location based on IP:</b><br>Latitude: ${latitude}<br>Longitude: ${longitude}`)
+                .openPopup();
+        }).fail(function () {
+            $("#gfg").html("Failed to retrieve location details");
+        });
+    }).fail(function () {
+        $("#gfg").html("Failed to retrieve IP address");
+    });
+});
